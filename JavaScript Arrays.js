44520c1ea4d0825b306b1dd050b96177de3aaf2e
Atikur <coc13259@gmail.com>
2023-04-27 15:03:32 +0600

    JavaScript Syntax

--- JavaScript Arrays.js
+++ JavaScript Arrays.js
@@ -10,7 +10,15 @@ const cars = new Array("Saab", "Volvo", "BMW");
 cars.sort()
 console.log(cars)
 
+//Use the same trick to sort an array ascending:
 const points = [40, 100, 1, 5, 25, 10];
 points.sort(function(a, b){return a - b});
+
+console.log(points)
+
+
+//Use the same trick to sort an array descending:
+//const points = [40, 100, 1, 5, 25, 10];
+points.sort(function(a, b){return b - a});
 
 console.log(points)
